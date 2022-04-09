e2a6886c4923270629c1acfe1206730b285b1de5
falovic <fatihsahin.dev@gmail.com>
2022-04-09 12:34:34 +0300

    update

---
+++ greeter.js
@@ -0,0 +1,5 @@
+function greeter(name) {
+    console.log("Hello ".concat(name));
+}
+var user = 'Fatih';
+greeter(user);
---
+++ greeter.ts
@@ -0,0 +1,9 @@
+function greeter(name) {
+    console.log(`Hello ${name}`);
+}
+
+let user = 'Fatih';
+
+greeter(user);
+
+// tsc greeter.ts (js dosyası oluşturur)
